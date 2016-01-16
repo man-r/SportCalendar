48aa616e5e989dcc308d11ad1d33d98075befb4e
man-r <man-r@github.com>
2016-01-17 00:31:15 +0300

    adding test code for repeating alarm

--- android/src/com/man_r/sportcalendar/MyMainActivity.java
+++ android/src/com/man_r/sportcalendar/MyMainActivity.java
@@ -4,87 +4,40 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.util.*;
-import java.io.*;
-import java.net.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MyMainActivity extends Activity
 {
-    TextView textView;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
+  SampleAlarmReceiver alarm = new SampleAlarmReceiver();
 
-        textView = new TextView(this);
-        textView.setText("hello!");
-        try {
-          textView.setText(fromURL("http://m.kooora.com").toString());
-        } catch (Exception e) {
-          textView.setText(e.toString());
-        }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+  }
 
-        setContentView(textView);
-    }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+  }
 
-    static ArrayList<String> fromURL(String surl) {
-      URL url;
-      InputStream is = null;
-      BufferedReader br;
-      String line;
-      ArrayList<String> lines = new ArrayList<String>();
-
-      try {
-        url = new URL(surl);
-        is = url.openStream();  // throws an IOException
-        br = new BufferedReader(new InputStreamReader(is));
-
-        while ((line = br.readLine()) != null) {
-          if (lines.size() > 0 || line.contains("match_box")) {
-            lines.add(line);
-          }
-        }
-      } catch (MalformedURLException mue) {
-        mue.printStackTrace();
-      } catch (IOException ioe) {
-        ioe.printStackTrace();
-      } finally {
-        try {
-          if (is != null) is.close();
-        } catch (IOException ioe) {
-            // nothing to see here
-        }
+  // Menu options to set and cancel the alarm.
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+          // When the user clicks START ALARM, set the alarm.
+          case R.id.start_action:
+              alarm.setAlarm(this);
+              return true;
+          // When the user clicks CANCEL ALARM, cancel the alarm.
+          case R.id.cancel_action:
+              alarm.cancelAlarm(this);
+              return true;
       }
-
-      return lines;
-    }
-
-    static ArrayList<String> getUrlSource(String url) throws IOException {
-      URL mURL = new URL(url);
-      URLConnection mURLConnection = mURL.openConnection();
-
-      BufferedReader in = new BufferedReader(new InputStreamReader(mURLConnection.getInputStream(), "UTF-8"));
-      String inputLine;
-
-      StringBuilder a = new StringBuilder();
-      ArrayList<String> lines = new ArrayList<String>();
-      while ((inputLine = in.readLine()) != null) {
-        a.append(inputLine);
-        if (lines.size() > 0 || inputLine.contains("match_box")) {
-          lines.add(inputLine);
-          System.out.println(inputLine);
-        }
-
-        if (inputLine.contains("var video_list")) {
-          break;
-        }
-      }
-
-
-      in.close();
-
-
-      return lines;
-    }
+      return false;
+  }
 }
